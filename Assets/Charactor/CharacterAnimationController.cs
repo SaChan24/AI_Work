8ab9580da47f7c67da832855d46bd3ab082145e9
SaChan24 <taripoom_2224@hotmail.com>
2025-09-13 02:10:33 +0700

    Animation

---
+++ Assets/Charactor/CharacterAnimationController.cs
@@ -0,0 +1,60 @@
+// 13/9/2568 AI-Tag
+// This was created with the help of Assistant, a Unity Artificial Intelligence product.
+
+// 13/9/2568 AI-Tag
+// This was created with the help of Assistant, a Unity Artificial Intelligence product.
+
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        // Get the Animator component
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Check for R key press
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayAnimation("Agree_Gesture");
+        }
+
+        // Check for T key press
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            PlayAnimation("Dead");
+        }
+
+        // Check for F key press
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            PlayBlendTreeAnimation();
+        }
+    }
+
+    private void PlayAnimation(string animationName)
+    {
+        // Reset all triggers and stop Blend Tree
+        animator.ResetTrigger("Agree_Gesture");
+        animator.ResetTrigger("Dead");
+        animator.SetFloat("New Float", 0); // Reset Blend Tree parameter
+
+        // Play the new animation
+        animator.SetTrigger(animationName);
+    }
+
+    private void PlayBlendTreeAnimation()
+    {
+        // Reset all triggers to stop other animations
+        animator.ResetTrigger("Agree_Gesture");
+        animator.ResetTrigger("Dead");
+
+        // Trigger the Blend Tree animation
+        animator.SetTrigger("PlayBlendTree");
+    }
+}
